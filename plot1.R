7f48e4d877763dc508fa5a1d2370d090139bd90b
rseeto <ryan.seeto@alum.utoronto.ca>
2014-08-08 17:44:13 -0400

    Added comments

--- plot1.R
+++ plot1.R
@@ -1,10 +1,16 @@
+##Code to emulate Plot 1 from Course Project 1 of "Exploratory Data Analysis"
+
+##checks to see if data is available
 if(!file.exists("household_power_consumption.txt")){
     download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "exdata_data_household_power_consumption.zip")
     unzip("exdata_data_household_power_consumption.zip")
 }
 
+##reads and subsets data
 foo <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 foo1 <- subset(foo, Date == "1/2/2007" | Date == "2/2/2007")
+
+##creates png from plot
 png(file = "plot1.png", width = 480, height = 480, units = "px")
 hist(foo1$Global_active_power, col = "red", xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
 dev.off()
--- plot4.R
+++ plot4.R
@@ -1,12 +1,17 @@
+##Code to emulate Plot 4 from Course Project 1 of "Exploratory Data Analysis"
+
+##checks to see if data is available
 if(!file.exists("household_power_consumption.txt")){
     download.file("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile = "exdata_data_household_power_consumption.zip")
     unzip("exdata_data_household_power_consumption.zip")
 }
 
+##reads and subsets data
 foo <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings="?", colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
 foo1 <- subset(foo, Date == "1/2/2007" | Date == "2/2/2007")
 foo1$Date <- strptime(paste(foo1$Date,foo1$Time), "%d/%m/%Y %H:%M:%S")
 
+##creates png from plot
 png(file = "plot4.png", width = 480, height = 480, units = "px")
 par(mfcol = c(2, 2))
 plot(foo1$Date, foo1$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
