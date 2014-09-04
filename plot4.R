90f224d4c6767caf93c6274cc63fd5d004573790
rseeto <ryan.seeto@alum.utoronto.ca>
2014-09-04 19:10:50 -0400

    Removed "kilowatts" from top left graph

--- plot4.R
+++ plot4.R
@@ -14,7 +14,7 @@ foo1$Date <- strptime(paste(foo1$Date,foo1$Time), "%d/%m/%Y %H:%M:%S")
 ##creates png from plot
 png(file = "plot4.png", width = 480, height = 480, units = "px")
 par(mfcol = c(2, 2))
-plot(foo1$Date, foo1$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+plot(foo1$Date, foo1$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power")
 plot(foo1$Date, foo1$Sub_metering_1, type = "l", xlab = "", ylab = "Energy sub metering")
 lines(foo1$Date, foo1$Sub_metering_2, type = "l", col = "red")
 lines(foo1$Date, foo1$Sub_metering_3, type = "l", col = "blue")
